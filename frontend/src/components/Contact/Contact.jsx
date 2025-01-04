2df8cdf16b6db47719ec526301e1ca5815194567
Mahak Mehta <mahakmehta141106@gmail.com>
2025-01-04 19:21:19 +0530

    Changing the url's

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:3000/connect", { // Ensure the correct backend URL here
+      const response = await fetch("https://portfolio-8wrl.onrender.com/connect", { // Ensure the correct backend URL here
         method: "POST",
         headers: {
           "Content-Type": "application/json",
